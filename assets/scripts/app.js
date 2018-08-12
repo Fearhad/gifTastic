59e514de5b057508aff91034028c6d3b245a4581
Fearhad <allan.hartigan@gmail.com>
2018-08-11 20:12:11 -0400

    added API call function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,2 +1,44 @@
 var topics = ["Sweden", "Iceland", "Denmark", "Norway", "Finland"];
 
+function renderButtons() {
+
+    $("#btnContainer").empty();
+
+    for (var i = 0; i < topics.length; i++) {
+        var btn = $("<button>");
+        btn.addClass("country");
+        btn.attr("data-name", topics[i]);
+        btn.text(topics[i]);
+        $("#btnContainer").append(btn);
+    }
+};
+
+$(document).on( "click", "button", function() {
+    var country = $(this).attr("data-name");
+    console.log(country);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        country + "&api_key=tFS9qiGX2UQszVY0UPQkFtxl8gW3gYNR&limit=10";
+    $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function (response) {
+            var results = response.data;
+            console.log(results);
+
+            for (var i = 0; i < results.length; i++) {
+                var countryDiv = $("<div>");
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                var countryImage = $("<img>");
+                countryImage.attr("src", results[i].images.fixed_height.url);
+                countryDiv.append(p);
+                countryDiv.append(countryImage);
+                $("#imgContainer").prepend(countryDiv);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+  });
+
+renderButtons();
